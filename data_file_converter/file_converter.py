79ff8400a5177392026409cea66d2175d423ebac
Venkatesh Murugadas <venkateshdas.me@gmail.com>
2022-12-02 16:34:44 +0100

    Initial files

---
+++ data_file_converter/file_converter.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from io import BytesIO
+from typing import Tuple
+
+
+class DataFileConverter:
+
+    def __init__(self):
+        self.supported_file_types = ['csv', 'json', 'xlsx', 'parquet', 'feather', 'pickle', 'pkl', 'hdf', 'html', 'xml']
+
+    def _detect_file_type(self, file_path: str) -> str:
+        file_type = file_path.split('.')[-1]
+        return file_type
+
+    def _extract_file_name(self, file_path: str) -> str:
+        file_name = file_path.split('.')[0]
+        return file_name
+
+    # A function to convert any file format to dataframe in pandas. It accepts file path and file types as arguments and returns a dataframe.
+    def convert_to_dataframe(self, input_file_path: BytesIO, file_type: str) -> pd.DataFrame:
+        if file_type == 'xlsx':
+            df = pd.read_excel(input_file_path)
+        elif file_type in self.supported_file_types:
+            df = getattr(pd, f'read_{file_type}')(input_file_path)
+        else:
+            raise ValueError('File type not supported')
+        return df
+
+    # A function to convert any dataframe to any file format. It accepts dataframe, file path and file types as arguments and returns a dataframe.
+    def convert_to_file(self, df: pd.DataFrame, output_file_name: str, file_type: str):
+        if file_type == 'xlsx':
+            df.to_excel(f"{output_file_name}.{file_type}")
+        elif file_type in self.supported_file_types:
+            return getattr(df, f'to_{file_type}')(f"{output_file_name}.{file_type}")
+        else:
+            raise ValueError('File type not supported')
+
+    def convert_file(self, input_file_path: BytesIO, output_file_type: str) -> Tuple:
+        # streamlit specific code for reading file name from the file uploader widget
+        input_file_address = input_file_path.name
+        input_file_name = self._extract_file_name(input_file_address)
+        input_file_type = self._detect_file_type(input_file_address)
+        df = self.convert_to_dataframe(input_file_path, input_file_type)
+        output_file = self.convert_to_file(df, input_file_name, output_file_type)
+        output_file_name = f"{input_file_name}.{output_file_type}"
+
+        return output_file, output_file_name
